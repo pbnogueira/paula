ab417392c02a4a34e5110107dffb35041c4e5d49
pbnogueira <84620831+pbnogueira@users.noreply.github.com>
2021-05-22 11:22:42 +0100

    Atualização main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,18 @@
-basic.forever(function () {
-    basic.showIcon(IconNames.Happy)
-    basic.showIcon(IconNames.Sad)
-    basic.showIcon(IconNames.Confused)
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Meh)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showArrow(ArrowNames.North)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showNumber(0)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showNumber(10)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("Bom dia")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
 })
